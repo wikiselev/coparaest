3fe3bfd0b4be85f9fcdcba672541768781cc75b8
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2014-08-18 17:33:11 +0100

    updated plots.R

--- scripts/plots.R
+++ scripts/plots.R
@@ -11,46 +11,32 @@ all.est <- NULL
 # import the data table with estimated wt time courses
 wt_est <- read.table(paste("wt_est.txt", sep=""), 
 	header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-time.step.num <- length(wt_est[ ,1])
-colnames(wt_est) <- c("time", "pi45p2", "pi3k_act", "pten", "pip3", "pi3k", "lig", 
-	"rec", "pi45p2_pi3k_act", "pip3_pten", "rec_lig", "rec_dimer", "rec_phosp")
-wt_est <- melt(wt_est)
-wt_est <- wt_est[(time.step.num + 1):length(wt_est[ ,1]),]
-wt_est$time <- rep(c(init.step:(time.step.num - 1 + init.step)), comp.num)
-wt_est$cond <- rep("wt", length(wt_est[ ,1]))
-wt_est$run <- rep("est", length(wt_est[ ,1]))
+wt_est <- melt(wt_est, id = "time")
+wt_est$cond <- "wt"
+wt_est$run <- "est"
 
 # import the data table with estimated pten time courses
 pten_est <- read.table(paste("pten_est.txt", sep=""), 
 	header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-time.step.num <- length(pten_est[ ,1])
-colnames(pten_est) <- c("time", "pi45p2", "pi3k_act", "pten", "pip3", "pi3k", "lig", 
-	"rec", "pi45p2_pi3k_act", "pip3_pten", "rec_lig", "rec_dimer", "rec_phosp")
-pten_est <- melt(pten_est)
-pten_est <- pten_est[(time.step.num + 1):length(pten_est[ ,1]),]
-pten_est$time <- rep(c(init.step:(time.step.num - 1 + init.step)), comp.num)
-pten_est$cond <- rep("pten", length(pten_est[ ,1]))
-pten_est$run <- rep("est", length(pten_est[ ,1]))
+pten_est <- melt(pten_est, id = "time")
+pten_est$cond <- "pten"
+pten_est$run <- "est"
 
 # import the data table with experimental wt time courses
 wt_exp <- read.table("wt_lit.txt", header = TRUE, sep = "\t", 
 	stringsAsFactors = FALSE)
-time.steps <- wt_exp$time
-wt_exp <- melt(wt_exp)
-wt_exp <- wt_exp[7:(length(wt_exp[ ,1]) - 6),]
-wt_exp$time <- rep(time.steps, 2)
-wt_exp$cond <- rep("wt", length(wt_exp[ ,1]))
-wt_exp$run <- rep("exp", length(wt_exp[ ,1]))
+wt_exp <- melt(wt_exp, id = "time")
+wt_exp$cond <- "wt"
+wt_exp$run <- "exp"
 
 # import the data table with experimental pten time courses
 pten_exp <- read.table("pten_lit.txt", header = TRUE, sep = "\t", 
 	stringsAsFactors = FALSE)
-time.steps <- pten_exp$time
-pten_exp <- melt(pten_exp)
-pten_exp <- pten_exp[7:(length(pten_exp[ ,1]) - 12),]
-pten_exp$time <- rep(time.steps, 2)
-pten_exp$cond <- rep("pten", length(pten_exp[ ,1]))
-pten_exp$run <- rep("exp", length(pten_exp[ ,1]))
+pten_exp <- melt(pten_exp, id = "time")
+pten_exp$cond <- "pten"
+pten_exp$run <- "exp"
+
+
 
 total.data <- rbind(wt_est, pten_est, wt_exp, pten_exp)
 all.est <- rbind(all.est, total.data)
