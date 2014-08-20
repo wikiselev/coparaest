f5411ab9e14067619edd52269f87c35016b63b72
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2014-08-20 13:04:14 +0100

    added queuing-system argument to 1-run-coparaest.sh

--- 1-run-coparaest.sh
+++ 1-run-coparaest.sh
@@ -9,8 +9,11 @@ for i in $(seq 1 $1)
 do
 	cp -r ../../model $i
 	cd $i
-	bsub sh ../../../scripts/cluster.sh
-	echo "sh ../../../scripts/cluster.sh" | qsub -cwd -V
+	if [ $2 = "lsf" ]; then
+	   bsub sh ../../../scripts/cluster.sh
+	else
+		if [ $2 = "sge" ]; then
+	   		echo "sh ../../../scripts/cluster.sh" | qsub -cwd -V
+	   	# else echo "Wrong or no queuing-system argument is provided! It should be either lsf or sge."
 	cd ..
 done
-
