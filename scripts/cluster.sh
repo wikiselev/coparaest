aea04f540236e925f23d817912ccbd479b75b446
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2015-05-15 15:52:02 +0100

    move get-obj-values.pl; introduce optional to cluster.sh

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# usage: bsub sh main.sh cps_file param_est_num best_obj_val_num
-# where:
-# cps_file -- the copasi model file
-# param_est_num -- the number of parameter estimation iteration
-# best_obj_val_num -- number of best objective values to consider
-
 file=`ls *.cps`
 
 CopasiSE -s $file $file
-perl ../../../scripts/exe-check-box.pl $file
-CopasiSE model-tmp.cps
-perl ../../../scripts/split-result-files.pl
-Rscript ../../../scripts/plots.R
+
+# the rest is optional:
+
+# # 1. run a Time Course task with a new parameter set
+# perl ../../../scripts/exe-check-box.pl $file
+# CopasiSE model-tmp.cps
+
+# # 2. plot the resulting time courses in pdf file
+# perl ../../../scripts/split-result-files.pl
+# Rscript ../../../scripts/plots.R
