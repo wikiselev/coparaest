106081bb032b85d5c2309fe1319cb7fea2ee6d93
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2015-05-15 15:45:32 +0100

    add out files

--- 1-run-coparaest.sh
+++ 1-run-coparaest.sh
@@ -7,13 +7,13 @@ cd results/param-estimations
 
 for i in $(seq 1 $1)
 do
-	cp -r ../../model $i
+  cp -r ../../model $i
 	cd $i
 	if [ $2 = "lsf" ]; then
-	   bsub sh ../../../scripts/cluster.sh
+	   bsub -o out -e err sh ../../../scripts/cluster.sh
 	fi
 	if [ $2 = "sge" ]; then
-   		echo "sh ../../../scripts/cluster.sh" | qsub -cwd -V
+   		echo "sh ../../../scripts/cluster.sh" | qsub -cwd -V -o out -e err
    	fi
 	cd ..
 done
